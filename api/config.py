babd38e59c91fcc9c25eb20aac84f49625411025
12734hs <12734@holbertonstudents.com>
2025-12-01 00:19:38 +0400

    Update config.py

--- api/config.py
+++ api/config.py
@@ -1,5 +1,6 @@
-api_key_holberton = 'c612c439d54a20c20ef53aecde0967c7'
-id_holberton = 12734
-tg_bot_api = '7864829034:AAGQjMroPR2bZG6YONKzj8zeDphQxyLSztM'
+api_key_holberton = ''
+id_holberton = 
+tg_bot_api = ''
 time = '09:00'
+
 
